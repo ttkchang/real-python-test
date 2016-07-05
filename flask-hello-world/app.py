462ec531507c340f7c728daaf1a7bcbe7fe11ce7
Tom Chang <ttkchang@hotmail.com>
2016-07-04 18:31:48 -0700

    added debug and other routes

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -5,7 +5,10 @@ from flask import Flask
 
 # create the application object
 app = Flask(__name__)
-# 
+#
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
@@ -14,7 +17,40 @@ app = Flask(__name__)
 # define the view using a function which returns a string
 # 
 def hello_world():
-	return "Hello World!"
+	return "Hello World!???"
+
+@app.route("/maybe")
+def maybe_world():
+	return "Maybe World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integervalue/<int:value>")
+def int_type(value):
+	print "this is in the terminal :" +value + 1
+	return "Correct Integer"
+
+@app.route("/floatvalue/<float:value>")
+def float_type(value):
+	print "this is in the terminal :" +value + 1
+	return "Correct Float"
+
+@app.route("/pathvalue/<path:value>")
+def path_type(value):
+	print "this is in the terminal :" +value
+	return "Correct Path"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name), 200
+	else :
+		return "Not Found", 404
+
+
 
 #start the development server using the run() method
 if __name__ == "__main__":
